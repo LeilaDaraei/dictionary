8b912266ff949c5e9aa59163e27d27a4e5def79d
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-19 15:49:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,5 +11,3 @@ function App() {
     </div>
   );
 }
-
-export default App;

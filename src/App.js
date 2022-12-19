3583aa4c730c27f4f6d899c25518820a427081c1
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-19 16:32:37 +0100

    updated search engine on events

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
+import React from "react";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary App</h1>
-        <Dictionary />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          {" "}
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Leila Daraei and is{" "}
+          <a href="https://github.com/LeilaDaraei/dictionary">
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
-      <form>
-        <input type="search" placeholder="enter a word" />
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="enter a word"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        />
         <input type="submit" value="Search" />
       </form>
     </div>

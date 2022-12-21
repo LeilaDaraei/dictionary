9f7f3e31fbf63c526088e8253f49ac64b10bb03a
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-21 17:27:00 +0100

    added synonym

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,24 +5,18 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        {props.meaning.definitions.map((definition, index) => {
-          return (
-            <div key={index}>
-              <p>
-                <strong>Definition:</strong>
-                {definition.definition}
-                <br />
-                <strong>Example:</strong>
-                <em>{definition.example}</em>
-                <br />
-                <Synonyms synonysm={definition.synonyms} />
-                <br />
-              </p>
-            </div>
-          );
-        })}
-      </p>
+      {props.meaning.definitions.map((definition, index) => {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }

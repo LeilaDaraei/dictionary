f6f338d65dce1ea4d783a331f78df8f893ed6ad6
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-21 21:08:32 +0100

    fixed the audio/text relations

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
-  return (
-    <div className="Phonetic">
-      <div>{props.phonetic.text}</div>
-      <a href={props.phonetic.audio}>listen</a>
-    </div>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        {props.phonetic.text} <br />
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+      </div>
+    );
+  } else {
+    return <div>{props.phonetic.text}</div>;
+  }
 }

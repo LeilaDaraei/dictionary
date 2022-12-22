5dd04631ebb7a85b26e7cc68bf575832609d508c
Leila Daraei <107926155+LeilaDaraei@users.noreply.github.com>
2022-12-22 19:21:22 +0100

    added the icon

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Phonetic.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
 export default function Phonetic(props) {
   if (props.phonetic.audio) {
     return (
       <div className="Phonetic">
         <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-          Listen
+         <FontAwesomeIcon icon={faCirclePlay} />
         </a>{" "}
+        
         <span className="text">{props.phonetic.text}</span>
       </div>
     );
